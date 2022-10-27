4f20c310c9fc5cdf26beb5c13fe2cef0431fb6ae
Danik <danik.kanavalov@mail.ru>
2022-10-27 22:15:45 +0400

    UpDate

--- Front-end(React)/src/index.js
+++ Front-end(React)/src/index.js
@@ -9,23 +9,6 @@ import cookie from "js-cookie"
 
 
 let token = cookie.get('token');
-
-// const JwtToken = 'nqJKpDf1tQOx4hjwm2e0B2HKJECdSuhS0PqSgc0oyjYOA3NSq2EHwJ4ZKtimcIcD';
-console.log(token);
-// jwt.verify(token, JwtToken, (err, decoded) => {
-//     if(err)
-//     {
-//       token = null
-//       cookie.remove('token')
-//     }
-//     else {
-//       if(decoded.iss !== 'http://127.0.0.1:8000/api/auth/login')
-//       {
-//         token = null
-//         cookie.remove('token')
-//       }
-//     }
-// });
 const render = ()=> {
   
   const root = ReactDOM.createRoot(document.getElementById('root'));
